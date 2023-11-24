5aeadbad407e7008055f8f94cf748c293399286d
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-24 19:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	const alphanumericChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+
+  for (let i = 0; i < l; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+    result += alphanumericChars.charAt(randomIndex);
+  }
+
+  return result;
 }
 
 // Do not change the code below.
